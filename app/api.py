401668f49e61fcb07ecc11d7a2e96a06f09d5fb1
gllimas <dilaratarasova158@gmail.com>
2025-05-28 23:00:05 +0300

    add book creat, read, update, delete

--- app/api.py
+++ app/api.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 
 from sqlmodel import Session
 from starlette import status
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
 from starlette.responses import JSONResponse, StreamingResponse, Response, FileResponse
 from starlette.status import HTTP_202_ACCEPTED
 
@@ -10,9 +12,7 @@ from database import get_session
 
 
 from pydantic import BaseModel
-from typing import Optional
-
-
+from typing import Optional, List
 
 router = APIRouter()
 
@@ -24,6 +24,14 @@ class BookCreate(BaseModel):
     isbn: Optional[str] = None
     quantity: int = 1
 
+
+class BookResponse(BaseModel):
+    id: int
+    bookname: str
+    author: str
+    year_of_publication: int
+    isbn: Optional[str]
+    quantity: int
 
 @router.post("/creat_book")
 def create_book(book: BookCreate, db: Session = Depends(get_session)):
@@ -40,7 +48,48 @@ def create_book(book: BookCreate, db: Session = Depends(get_session)):
     return Response(status_code=status.HTTP_201_CREATED)
 
 
-@router.get("/read_books")
-def read_books(db: Session = Depends(get_session)):
-    db_books = db.query(Books).all()
+@router.get("/read_books", response_model=List[BookResponse])
+def read_books(
+        id: Optional[int] = Query(None),
+        bookname: Optional[str] = Query(None),
+        db: Session = Depends(get_session)
+):
+    query = db.query(Books)
+
+    if id is not None:
+        query = query.filter(Books.id == id)
+    if bookname is not None:
+        query = query.filter(
+            Books.bookname.ilike(f"%{bookname}%"))
+
+    db_books = query.all()
+
+    if not db_books:
+        raise HTTPException(status_code=404, detail="Book not found")
+
     return db_books
+
+
+@router.put("/books/{book_id}", response_model=BookResponse)
+async def update_book(book_id: int, book: BookCreate, db: Session = Depends(get_session)):
+    db_book = db.query(Books).filter(Books.id == book_id).first()
+    if not db_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    for key, value in book.dict().items():
+        setattr(db_book, key, value)
+
+    db.commit()
+    db.refresh(db_book)
+
+    return Response(status_code=status.HTTP_200_OK)
+
+
+@router.delete("/books/{book_id}", response_model=BookResponse)
+def delete_book(book_id: int, db: Session = Depends(get_session)):
+    db_book = db.query(Books).filter(Books.id == book_id).first()
+    if not db_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    db.delete(db_book)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
